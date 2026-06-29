890daba0147dc686938799070fd54229a63d673d
EDITz <7481152+EDITzDev@users.noreply.github.com>
2026-06-29 23:31:09 +0100

    Add missing migration for MySql
    Related commit cb953fffb703027690ee3c8ae05731d26797773c

---
+++ src/Sanctuary.Database.MySql/Migrations/20260629185011_AddPlayTimeColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sanctuary.Database.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayTimeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayTime",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlayTime",
+                table: "Characters");
+        }
+    }
+}
