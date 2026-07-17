9984dd72292f3b2502b62e597fe6303339ed4c58
CarterW24 <cweakley@txautosys.com>
2026-07-17 17:19:29 -0500

    Update src/Sanctuary.Packet/BasePlayerUpdatePacket/PlayerUpdatePacketRemoveNotifications.cs
    Co-authored-by: EDITz <7481152+EDITzDev@users.noreply.github.com>

--- src/Sanctuary.Packet/BasePlayerUpdatePacket/PlayerUpdatePacketRemoveNotifications.cs
+++ src/Sanctuary.Packet/BasePlayerUpdatePacket/PlayerUpdatePacketRemoveNotifications.cs
@@ -4,28 +4,41 @@ using Sanctuary.Core.IO;
 
 namespace Sanctuary.Packet;
 
-public class PlayerUpdatePacketRemoveNotifications : ISerializablePacket
+public class PlayerUpdatePacketRemoveNotifications : BasePlayerUpdatePacket, ISerializablePacket
 {
-    public const short OpCode = 35;
-    public const short SubOpCode = 11;
+    public new const short OpCode = 11;
 
-    public List<ulong> Guids = new();
+    public class Entry : ISerializableType
+    {
+        public ulong Guid;
+
+        public int ThoughtBubbleIconId;
+
+        public int CompositeEffectId;
+
+        public void Serialize(PacketWriter writer)
+        {
+            writer.Write(Guid);
+
+            writer.Write(ThoughtBubbleIconId);
+
+            writer.Write(CompositeEffectId);
+        }
+    }
+
+    public List<Entry> Entries = [];
+
+    public PlayerUpdatePacketRemoveNotifications() : base(OpCode)
+    {
+    }
 
     public byte[] Serialize()
     {
         using var writer = new PacketWriter();
 
-        writer.Write(OpCode);
-        writer.Write(SubOpCode);
+        Write(writer);
 
-        writer.Write(Guids.Count);
-
-        foreach (var guid in Guids)
-        {
-            writer.Write(guid);
-            writer.Write(0);
-            writer.Write(0);
-        }
+        writer.Write(Entries);
 
         return writer.Buffer;
     }
