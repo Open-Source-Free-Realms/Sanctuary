a24ce6bf7024d62af0d443faf07fea11c960c872
Alexander Koldy <alexanderkoldy.ak@gmail.com>
2026-09-06 01:09:07 -0400

    Reorganized triggers when OnClientIsReady is fired.

--- src/Sanctuary.Game/Zones/HousingZone.cs
+++ src/Sanctuary.Game/Zones/HousingZone.cs
@@ -1,5 +1,6 @@
 using System;
 
+using Sanctuary.Game.Entities;
 using Sanctuary.Game.Resources.Definitions.Zones;
 
 namespace Sanctuary.Game.Zones;
@@ -9,5 +10,12 @@ public sealed class HousingZone : BaseZone
     public HousingZone(HousingZoneDefinition zoneDefinition, IServiceProvider serviceProvider)
         : base(zoneDefinition, serviceProvider)
     {
+    }
+
+    public override void OnClientIsReady(Player player)
+    {
+        base.OnClientIsReady(player);
+
+        SendShopData(player);
     }
 }
