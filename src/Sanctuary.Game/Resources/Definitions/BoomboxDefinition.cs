eebaca08dc973b83059722fbbb485f2031230ad8
JadenY <jadeny43571@gmail.com>
2026-09-13 10:34:17 -0500

    Transform Realms Roll boombox dancers into Rick

--- src/Sanctuary.Game/Resources/Definitions/BoomboxDefinition.cs
+++ src/Sanctuary.Game/Resources/Definitions/BoomboxDefinition.cs
@@ -6,4 +6,5 @@ public class BoomboxDefinition
     public int ModelId { get; set; }
     public int EffectId { get; set; }
     public int[] DanceSequence { get; set; } = [];
+    public int TransformModelId { get; set; }
 }
